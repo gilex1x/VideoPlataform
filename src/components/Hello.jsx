a787ff1269d8afcbd5271e8a2fbbc553a9eb0603
GILEXX <gilbertosa099@outlook.com>
2020-08-23 14:45:41 -0500

    First commit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,7 @@
+import React  from 'react';
+
+const Hello =()=>{
+  <h1>Hola, esto es un coponente</h1>
+};
+
+export default Hello; 
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/Hello';
+//Render recibe, el componente y donde lo va a poner
+ReactDOM.render(<Hello/>,document.getElementById(app));
